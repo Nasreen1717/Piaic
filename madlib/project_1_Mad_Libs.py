16b112f80fa6eabbc52023d93099b99848e7310d
Nasreen Mahtab <hoorainmahtab@gmail.com>
2024-12-23 20:17:24 +0500

    first commit

---
+++ madlib/project_1_Mad_Libs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+madlib = f"Computer Programming is so {adj}! It makes me excited all the time because I love to {verb1}. Stay hydrated and act like you are a {famous_person}!"
+print(madlib)
